297677573ac7a0f6ed12c6bf5be4d990ac00b55c
rinkuberde <rinkuberde@gmail.com>
2023-01-23 11:24:05 +0530

    [Rinku] -  Welcome Message

---
+++ Day-15-BinarySearchTree/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day_15_BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welvome to Binary Search Tree Practice problems!!!");
+        }
+    }
+}
