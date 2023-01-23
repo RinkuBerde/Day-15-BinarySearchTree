dab265ca48aaf3f8857c149ca25a06f9f7a6c332
rinkuberde <rinkuberde@gmail.com>
2023-01-23 11:40:59 +0530

    [Rinku] - create a BST and add 56 and 30 & 70

--- Day-15-BinarySearchTree/Program.cs
+++ Day-15-BinarySearchTree/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welvome to Binary Search Tree Practice problems!!!");
+            Console.WriteLine("Welcome to Binary Search Tree Practice problems!!!");
+            Console.WriteLine("\n1. Create Binary Search Tree byadding 56 and then adding 30 & 70");
+            Console.Write("Enter your choice : ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            switch (option)
+            {
+                case 1:
+                    binarySearch.Insert(30);
+                    binarySearch.Insert(70);
+                    binarySearch.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid option!!!");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
+    
